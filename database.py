a6b00bd0be4f2722d41d4d55a906e40a20072bbd
wh0smarg0 <wh0smarg0.s@gmail.com>
2026-03-12 15:36:53 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,30 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 
-# Налаштування підключення до PostgreSQL
-DB_CONFIG = {
-    "dbname": "taxes_db",
-    "user": "postgres",
-    "password": "masterkey",
-    "host": "127.0.0.1",
-    "port": "5432"
-}
+# Налаштування підключення
+MONGO_DETAILS = "mongodb://localhost:27017"
 
-def get_db_connection():
-    conn = psycopg2.connect(**DB_CONFIG)
-    return conn
+client = AsyncIOMotorClient(MONGO_DETAILS)
+db = client.taxes_nosql_db  # Назва нової бази даних
 
-def init_db():
-    """Створення таблиць при завантаженні застосунку"""
-    conn = get_db_connection()
-    with conn.cursor() as cur:
-        # Створення таблиці Платників
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS taxpayers (
-                id SERIAL PRIMARY KEY,
-                full_name VARCHAR(255) NOT NULL,
-                tin VARCHAR(10) UNIQUE NOT NULL
-            );
-        """)
-        # Створення таблиці Типів податків
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS tax_types (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL,
-                rate FLOAT NOT NULL
-            );
-        """)
-        # Створення таблиці Нарахувань
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS tax_records (
-                id SERIAL PRIMARY KEY,
-                amount FLOAT NOT NULL,
-                taxpayer_id INTEGER REFERENCES taxpayers(id) ON DELETE CASCADE,
-                tax_type_id INTEGER REFERENCES tax_types(id)
-            );
-        """)
-        conn.commit()
-    conn.close()
+# Колекції (аналог таблиць у PostgreSQL)
+taxpayers_collection = db.get_collection("taxpayers")
+records_collection = db.get_collection("tax_records")
+tax_types_collection = db.get_collection("tax_types")
+
+# Функція для перетворення MongoDB-документа у зручний для Python словник
+def taxpayer_helper(taxpayer) -> dict:
+    return {
+        "id": str(taxpayer["_id"]),
+        "full_name": taxpayer["full_name"],
+        "tin": taxpayer["tin"],
+    }
+
+def record_helper(record) -> dict:
+    return {
+        "id": str(record["_id"]),
+        "amount": record["amount"],
+        "taxpayer_name": record.get("taxpayer_name"), # Дані тепер можуть бути вкладеними
+        "tax_name": record.get("tax_name"),
+        "rate": record.get("rate")
+    }
